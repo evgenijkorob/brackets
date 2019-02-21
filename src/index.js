e9387aa6e8fcb058b8962c1e3c73f33c781382e1
Eugene Korobeinikov <evgenij.k.wie@gmail.com>
2019-02-21 15:22:52 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+const OPENING = 0, CLOSING = 1;
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split("");
+  if (str.length < 2) {
+    return false;
+  }
+  let brackets = [], currBracketParam, lastBracketParam;
+  for (let i = 0; i < str.length; i++) {
+    currBracketParam = setBracketParam.call(bracketsConfig, str[i]);
+    if (i > 0) {
+      lastBracketParam = setBracketParam.call(bracketsConfig, brackets[brackets.length - 1]);
+      if (isPair.call(bracketsConfig, lastBracketParam, currBracketParam)) {
+        brackets.pop();
+        continue;
+      }
+    }
+    if (currBracketParam.state == OPENING) 
+      brackets.push(str[i]);
+    else 
+      return false;
+  }
+  return !(brackets.length > 0);
+}
+
+function setBracketParam(bracket) {
+  for (let i = 0; i < this.length; i++) {
+    switch(bracket) {
+      case (this[i][OPENING]):
+        return {type: i, state: OPENING};
+      case (this[i][CLOSING]):
+        return {type: i, state: CLOSING};
+    }
+  }
+  return {type: -1, state: undefined};
+}
+
+function isPair(first, second) {
+  if (first.type != second.type)
+    return false;
+  if (this[first.type][OPENING] == this[second.type][CLOSING])
+    return true;
+  return first.state == OPENING && second.state == CLOSING;
 }
